156d2d6b10fbd3f4e8c8bd9710118df961e801e9
Erhan <erhan@Erhans-Air.home>
2022-06-01 17:39:24 +0300

    major changes
    controlling the gui which working on raspberry pi function added

--- public/js/script.js
+++ public/js/script.js
@@ -8,6 +8,7 @@ $(document).ready(() => {
     const webSocket = new WebSocket(protocol + location.host);
 
     webSocket.onmessage = function onMessage(message) {
+
       try {
 
         connectionState = document.getElementById('deviceStatus')
@@ -16,8 +17,6 @@ $(document).ready(() => {
         const messageData = JSON.parse(message.data);
         console.log(messageData)
 
-        //document.querySelector('.table').removeChild(document.querySelector('.table').lastChild)
-        //document.querySelector('.table').replaceWith(makeTable(messageData.IotData))
         document.querySelector('.table').innerHTML= '';
         document.querySelector('.table').appendChild(makeTable(messageData.IotData))
 
@@ -51,18 +50,49 @@ window.onload = function () {
 
   writeButton =  document.querySelector('#writeregisterbutton')
 
-  writeButton.addEventListener('click', function() {
+  connectButton = document.querySelector('#changeDeviceIp')
 
-    console.log("Butona Basıldı")
+  connectButton.addEventListener('click', function() {
 
-    webSocketSendWriteRegisterData()
+    console.log("Connect butonuna basılıd")
+
+    webSocketSendDeviceIp()
 
   })
 
 
+  writeButton.addEventListener('click', function() {
+
+    console.log("Write Butonuna Basıldı")
+
+    webSocketSendWriteRegisterData()
+
+  })
   
 }
 
+var webSocketSendDeviceIp = function() {
+
+    const protocol = document.location.protocol.startsWith('https') ? 'wss://' : 'ws://';
+    const webSocket3 = new WebSocket(protocol + location.host)
+
+
+    deviceIp = document.querySelector('#inputIpAdress').value
+
+    console.log(deviceIp.toString())
+
+    data = JSON.stringify( {
+        method : 'changeDeviceIp',
+        deviceIp : deviceIp
+    })
+
+    webSocket3.onopen = function() {
+
+        webSocket3.send(data)
+
+    }
+    
+}
 
 
 var webSocketSendWriteRegisterData = function () {
@@ -104,34 +134,548 @@ function setRegisterIdtoReadOnlyInput(data){
 
 //////     MAKE TABLE FUNCTIONS ////////
 
-var sample_json = [{"IpAdress": "192.168.1.200", "RegisterId": 1, "RegisterValue": [0], "RegisterFunction":"Writable" },
+var sample_json = [
   {
-      "IpAdress": "192.168.1.200",
-      "RegisterId": 2,
-      "RegisterValue": [0], "RegisterFunction":"Writable" 
+      "Description": "Output frequency",
+      "ParameterNo": "M1.1",
+      "RegisterFunction": "Readable",
+      "RegisterId": "1",
+      "RegisterValue": null
   },
   {
-      "IpAdress": "192.168.1.200",
-      "RegisterId": 3,
-      "RegisterValue": [
-          0
-      ]
+      "Description": "Frequency reference",
+      "ParameterNo": "M1.2",
+      "RegisterFunction": "Readable",
+      "RegisterId": "24",
+      "RegisterValue": null
   },
   {
-      "IpAdress": "192.168.1.200",
-      "RegisterId": 4,
-      "RegisterValue": [
-          0
-      ]
+      "Description": "Motor speed",
+      "ParameterNo": "M1.3",
+      "RegisterFunction": "Readable",
+      "RegisterId": "2",
+      "RegisterValue": null
   },
   {
-      "IpAdress": "192.168.1.200",
-      "RegisterId": 5,
-      "RegisterValue": [
-          0
-      ]
+      "Description": "Motor current",
+      "ParameterNo": "M1.4",
+      "RegisterFunction": "Readable",
+      "RegisterId": "3",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Motor torque",
+      "ParameterNo": "M1.5",
+      "RegisterFunction": "Readable",
+      "RegisterId": "4",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Motor power",
+      "ParameterNo": "M1.6",
+      "RegisterFunction": "Readable",
+      "RegisterId": "5",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Motor voltage",
+      "ParameterNo": "M1.7",
+      "RegisterFunction": "Readable",
+      "RegisterId": "6",
+      "RegisterValue": null
+  },
+  {
+      "Description": "DC-link voltage",
+      "ParameterNo": "M1.8",
+      "RegisterFunction": "Readable",
+      "RegisterId": "7",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Unit temperature",
+      "ParameterNo": "M1.9",
+      "RegisterFunction": "Readable",
+      "RegisterId": "8",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Motor temperature",
+      "ParameterNo": "M1.10",
+      "RegisterFunction": "Readable",
+      "RegisterId": "9",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Latest fault code",
+      "ParameterNo": "M1.11",
+      "RegisterFunction": "Readable",
+      "RegisterId": "28",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Instant motor power",
+      "ParameterNo": "M1.12",
+      "RegisterFunction": "Readable",
+      "RegisterId": "1686",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Analog input 1",
+      "ParameterNo": "M2.1",
+      "RegisterFunction": "Readable",
+      "RegisterId": "10",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Keypad pot voltage",
+      "ParameterNo": "M2.2",
+      "RegisterFunction": "Readable",
+      "RegisterId": "1858",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Analog output",
+      "ParameterNo": "M2.3",
+      "RegisterFunction": "Readable",
+      "RegisterId": "25",
+      "RegisterValue": null
+  },
+  {
+      "Description": "DI1, DI2, DI3",
+      "ParameterNo": "M2.4",
+      "RegisterFunction": "Readable",
+      "RegisterId": "12",
+      "RegisterValue": null
+  },
+  {
+      "Description": "DI4",
+      "ParameterNo": "M2.5",
+      "RegisterFunction": "Readable",
+      "RegisterId": "13",
+      "RegisterValue": null
+  },
+  {
+      "Description": "RO1, RO2",
+      "ParameterNo": "M2.8",
+      "RegisterFunction": "Readable",
+      "RegisterId": "557",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Multi-monitoring",
+      "ParameterNo": "M9.1",
+      "RegisterFunction": "Readable",
+      "RegisterId": "30",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Keypad reference",
+      "ParameterNo": "R11",
+      "RegisterFunction": "Writable",
+      "RegisterId": "141",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Minimum frequency",
+      "ParameterNo": "P1.1",
+      "RegisterFunction": "Writable",
+      "RegisterId": "101",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Maximum frequency",
+      "ParameterNo": "P1.2",
+      "RegisterFunction": "Writable",
+      "RegisterId": "102",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Acceleration time 1",
+      "ParameterNo": "P1.3",
+      "RegisterFunction": "Writable",
+      "RegisterId": "103",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Deceleration time 1",
+      "ParameterNo": "P1.4",
+      "RegisterFunction": "Writable",
+      "RegisterId": "104",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Motor nominal current",
+      "ParameterNo": "P1.6",
+      "RegisterFunction": "Writable",
+      "RegisterId": "486",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Motor nominal speed",
+      "ParameterNo": "P1.7",
+      "RegisterFunction": "Writable",
+      "RegisterId": "489",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Motor power factor",
+      "ParameterNo": "P1.8",
+      "RegisterFunction": "Writable",
+      "RegisterId": "490",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Motor nominal voltage",
+      "ParameterNo": "P1.9",
+      "RegisterFunction": "Writable",
+      "RegisterId": "487",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Motor nominal frequency",
+      "ParameterNo": "P1.10",
+      "RegisterFunction": "Writable",
+      "RegisterId": "488",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Remote control place",
+      "ParameterNo": "P1.13",
+      "RegisterFunction": "Writable",
+      "RegisterId": "135",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Remote reference",
+      "ParameterNo": "P1.14",
+      "RegisterFunction": "Writable",
+      "RegisterId": "137",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Local control place",
+      "ParameterNo": "P1.11",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1695",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Local reference",
+      "ParameterNo": "P1.12",
+      "RegisterFunction": "Writable",
+      "RegisterId": "136",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Serial communication",
+      "ParameterNo": "P11.1.1",
+      "RegisterFunction": "Writable",
+      "RegisterId": "586",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Slave address",
+      "ParameterNo": "P11.2.1",
+      "RegisterFunction": "Writable",
+      "RegisterId": "587",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Baud rate",
+      "ParameterNo": "P11.2.2",
+      "RegisterFunction": "Writable",
+      "RegisterId": "584",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Parity type",
+      "ParameterNo": "P11.2.3",
+      "RegisterFunction": "Writable",
+      "RegisterId": "585",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Modbus RTU protocol status",
+      "ParameterNo": "P11.2.4",
+      "RegisterFunction": "Writable",
+      "RegisterId": "588",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Communication timeout modbus RTU",
+      "ParameterNo": "P11.2.5",
+      "RegisterFunction": "Writable",
+      "RegisterId": "593",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Modbus RTU fault response",
+      "ParameterNo": "P11.2.6",
+      "RegisterFunction": "Writable",
+      "RegisterId": "2516",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Bluetooth enabled",
+      "ParameterNo": "P11.6.1",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1895",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Bluetooth broadcast mode",
+      "ParameterNo": "P11.6.2",
+      "RegisterFunction": "Writable",
+      "RegisterId": "2920",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Bluetooth pairing reset",
+      "ParameterNo": "P11.6.3",
+      "RegisterFunction": "Writable",
+      "RegisterId": "2935",
+      "RegisterValue": null
+  },
+  {
+      "Description": "IP address mode",
+      "ParameterNo": "P12.1.1",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1500",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Active IP address",
+      "ParameterNo": "P12.1.2",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1507",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Active subnet mask",
+      "ParameterNo": "P12.1.3",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1509",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Active default gateway",
+      "ParameterNo": "P12.1.4",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1511",
+      "RegisterValue": null
+  },
+  {
+      "Description": "MAC address",
+      "ParameterNo": "P12.1.5",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1513",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Static IP address",
+      "ParameterNo": "P12.1.6",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1501",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Static subnet mask",
+      "ParameterNo": "P12.1.7",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1503",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Static default gateway",
+      "ParameterNo": "P12.1.8",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1505",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Ethernet communication timeout",
+      "ParameterNo": "P12.1.9",
+      "RegisterFunction": "Writable",
+      "RegisterId": "611",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Trusted IP white list",
+      "ParameterNo": "P12.2.1",
+      "RegisterFunction": "Writable",
+      "RegisterId": "68",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Trusted IP filter enable",
+      "ParameterNo": "P12.2.2",
+      "RegisterFunction": "Writable",
+      "RegisterId": "76",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Modbus TCP enable",
+      "ParameterNo": "P12.3.1",
+      "RegisterFunction": "Writable",
+      "RegisterId": "1942",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Modbus TCP connection limit",
+      "ParameterNo": "P12.3.2",
+      "RegisterFunction": "Writable",
+      "RegisterId": "609",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Modbus TCP unit identifier number",
+      "ParameterNo": "P12.3.3",
+      "RegisterFunction": "Writable",
+      "RegisterId": "610",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Modbus TCP protocol status",
+      "ParameterNo": "P12.3.4",
+      "RegisterFunction": "Writable",
+      "RegisterId": "612",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Modbus TCP fault response",
+      "ParameterNo": "P12.3.5",
+      "RegisterFunction": "Writable",
+      "RegisterId": "2517",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Web UI protocol status",
+      "ParameterNo": "P12.6.1",
+      "RegisterFunction": "Writable",
+      "RegisterId": "2915",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Web UI fault response",
+      "ParameterNo": "P12.6.2",
+      "RegisterFunction": "Writable",
+      "RegisterId": "2916",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Web UI communication timeout",
+      "ParameterNo": "P12.6.3",
+      "RegisterFunction": "Writable",
+      "RegisterId": "2919",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Web UI enable",
+      "ParameterNo": "P12.6.4",
+      "RegisterFunction": "Writable",
+      "RegisterId": "2921",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Language",
+      "ParameterNo": "P13.1.1",
+      "RegisterFunction": "Writable",
+      "RegisterId": "340",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Application",
+      "ParameterNo": "P13.1.2",
+      "RegisterFunction": "Writable",
+      "RegisterId": "142",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Parameter sets",
+      "ParameterNo": "P13.1.3",
+      "RegisterFunction": "Writable",
+      "RegisterId": "619",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Up to keypad (for remote keypad only)",
+      "ParameterNo": "P13.1.4",
+      "RegisterFunction": "Writable",
+      "RegisterId": "620",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Down from keypad (for remote keypad only)",
+      "ParameterNo": "P13.1.5",
+      "RegisterFunction": "Writable",
+      "RegisterId": "621",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Parameter comparison (for remote keypad only)",
+      "ParameterNo": "P13.1.6",
+      "RegisterFunction": "Writable",
+      "RegisterId": "623",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Fan control",
+      "ParameterNo": "P13.2.7",
+      "RegisterFunction": "Writable",
+      "RegisterId": "632",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Keypad software version",
+      "ParameterNo": "P13.4.1",
+      "RegisterFunction": "Readable",
+      "RegisterId": "640",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Motor control software version",
+      "ParameterNo": "P13.4.2",
+      "RegisterFunction": "Readable",
+      "RegisterId": "642",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Application software version",
+      "ParameterNo": "P13.4.3",
+      "RegisterFunction": "Readable",
+      "RegisterId": "644",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Software bundle version",
+      "ParameterNo": "P13.4.4",
+      "RegisterFunction": "Readable",
+      "RegisterId": "1714",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Serial number",
+      "ParameterNo": "P13.5.1",
+      "RegisterFunction": "Readable",
+      "RegisterId": "648",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Multi-monitor set (for remote keypad only)",
+      "ParameterNo": "P13.5.2",
+      "RegisterFunction": "Writable",
+      "RegisterId": "627",
+      "RegisterValue": null
+  },
+  {
+      "Description": "Drive application name",
+      "ParameterNo": "P13.5.4",
+      "RegisterFunction": "Readable",
+      "RegisterId": "2922",
+      "RegisterValue": null
   }
 ]
+
 
 function makeNewRegisterValues(json_objects_arr) {
 
@@ -205,7 +749,6 @@ function makeTBODY(json, keys) {
 
     }
 
-
     keys.forEach(function (key) {
 
       let td = document.createElement('td');
@@ -223,13 +766,13 @@ function makeTBODY(json, keys) {
 
 
 
-        let registerValueInput = document.createElement('input')
-        registerValueInput.className = "inputTableRegister";
-        registerValueInput.id = object['RegisterId']                // register id   *!*
+        // let registerValueInput = document.createElement('input')
+        // registerValueInput.className = "inputTableRegister";
+        // registerValueInput.id = object['RegisterId']                // register id   *!*
 
         td.textContent = object[key];
 
-        td.appendChild(registerValueInput);
+        // td.appendChild(registerValueInput);
 
         tr.appendChild(td);
 
--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const WebSocket = require('ws');
 const path = require('path');
 const EventHubReader = require('./scripts/event-hub-reader.js');
 
-const iotHubConnectionString = "HostName=modbus-tcp-iot.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=5ZCPyIUC7prmgWfQueBajDqSGtMUe6YZvwiiwYovB3A=";
+const iotHubConnectionString = "HostName=modbus-web-iot-hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=JHSImIa3wXsTRGSh9gg+URLhWRLR/HBhU9xebSVcSHo=";
 if (!iotHubConnectionString) {
   console.error(`Environment variable IotHubConnectionString must be specified.`);
   
 }
 console.log(`Using IoT Hub connection string [${iotHubConnectionString}]`);
 
-const eventHubConsumerGroup = "consumergroup1";
+const eventHubConsumerGroup = "consumergroup2";
 
 console.log(eventHubConsumerGroup);
 if (!eventHubConsumerGroup) {
@@ -27,64 +27,9 @@ app.use((req, res /* , next */) => {
 });
 
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+let server = http.createServer(app);
+let wss = new WebSocket.Server({ server });
 
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    // console.log("Client Message : " + message)
-
-    if (JSON.parse(message).method == 'writeRegister') {
-      console.log("WRITE REGISTER METHOD WORKING")
-
-      console.log("Coming data is" + JSON.parse(message).sendRegisterId, JSON.parse(message).sendRegisterValue)
-
-      sendRegisterId =  String(JSON.parse(message).sendRegisterId)
-      sendRegisterValue = String(JSON.parse(message).sendRegisterValue)
-
-      var Client = require('azure-iothub').Client;
-      var Message = require('azure-iot-common').Message;
-
-      var connectionString = "HostName=modbus-tcp-iot.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=5ZCPyIUC7prmgWfQueBajDqSGtMUe6YZvwiiwYovB3A=";
-      var targetDevice = 'mypi';
-
-      var serviceClient = Client.fromConnectionString(connectionString);
-
-      serviceClient.open(function (err) {
-        if (err) {
-          console.error('Could not connect: ' + err.message);
-        } else {
-          console.log('Service client connected');
-          var message = new Message(sendRegisterId+"," +sendRegisterValue);
-          console.log('Sending message: ' + message.getData());
-          serviceClient.send(targetDevice, message);
-          console.log("Buraya geldi")
-        }
-      });
-
-
-    }
-
-  })
-})
-
-
-wss.broadcast = (data) => {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      try {
-        console.log(`Broadcasting data ${data}`);
-        client.send(data);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  });
-};
-
-server.listen(process.env.PORT || '3000', () => {
-  console.log('Listening on %d.', server.address().port);
-});
 
 const eventHubReader = new EventHubReader(iotHubConnectionString, eventHubConsumerGroup);
 
@@ -103,5 +48,99 @@ const eventHubReader = new EventHubReader(iotHubConnectionString, eventHubConsum
     }
   });
 })().catch();
+
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log("Client Message : " + message)
+
+    if ( JSON.parse(message).method == 'changeDeviceIp' ) {
+
+      var Client2 = require('azure-iothub').Client;
+      var Message2 = require('azure-iot-common').Message;
+
+      var connectionString = "HostName=modbus-web-iot-hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=JHSImIa3wXsTRGSh9gg+URLhWRLR/HBhU9xebSVcSHo=";
+      var targetDevice = 'myPi';
+
+      var serviceClient2 = Client2.fromConnectionString(connectionString);
+
+      serviceClient2.open(function (err) {
+        if (err) {
+          console.error('Could not connect: ' + err.message);
+        } else {
+          console.log('Service client connected');
+          let deviceIpComesFromClient = String(JSON.parse(message).deviceIp)
+          var message2 = new Message2("changeDeviceIp" + "," + deviceIpComesFromClient)
+          console.log('Sending message: ' + message2.getData());
+          serviceClient2.send(targetDevice, message2);
+          console.log("Buraya geldi writeRegister")
+        }
+      });
+
+      console.log("Buraya geldi changeDeviceIp")
+
+    }
+
+    if (JSON.parse(message).method == 'writeRegister' ) {
+      console.log("WRITE REGISTER METHOD WORKING")
+
+      console.log("Coming data is" + JSON.parse(message).sendRegisterId, JSON.parse(message).sendRegisterValue)
+
+      sendRegisterId =  String(JSON.parse(message).sendRegisterId)
+      sendRegisterValue = String(JSON.parse(message).sendRegisterValue)
+
+      var Client = require('azure-iothub').Client;
+      var Message = require('azure-iot-common').Message;
+
+      var connectionString = "HostName=modbus-web-iot-hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=JHSImIa3wXsTRGSh9gg+URLhWRLR/HBhU9xebSVcSHo=";
+      var targetDevice = 'myPi';
+
+      var serviceClient = Client.fromConnectionString(connectionString);
+
+      serviceClient.open(function (err) {
+        if (err) {
+          console.error('Could not connect: ' + err.message);
+        } else {
+          console.log('Service client connected');
+          var message = new Message("changeRegisterValue"+","+sendRegisterId+"," +sendRegisterValue);
+          console.log('Sending message: ' + message.getData());
+          serviceClient.send(targetDevice, message);
+          console.log("Buraya geldi writeRegister")
+        }
+      });
+
+    }
+
+    // if(JSON.parse(message).method == 'stopCommunication') {
+
+    //   console.log("Stop Methodu")
+
+
+    //   // eventHubReader.stopReadMessage()
+
+    // }
+
+  })
+})
+
+
+wss.broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      try {
+        //console.log(`Broadcasting data ${data}`);
+        client.send(data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+};
+
+server.listen(process.env.PORT || '3030', () => {
+  console.log('Listening on %d.', server.address().port);
+});
+
+
 
 
